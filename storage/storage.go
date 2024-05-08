067847abb7d8b9853131417afa7d1e79bdbe63d0
Vsomera <vsomera77@outlook.com>
2024-05-07 20:26:21 -0700

    re-written addFruit logic (check if a row exists before adding fruit)

--- storage/storage.go
+++ storage/storage.go
@@ -49,9 +49,30 @@ func (s *MySqlStorage) GetFruitByName(name string) (*types.Fruit, error) {
 	return &fruit, nil
 }
 
+// AddFruit queries database to check if a row with the same name exists, if exists it adds the count to the existing row, if not it creates a new row with the name and count
 func (s *MySqlStorage) AddFruit(name string, count int) error {
+
+	// check if a row with the same name already exists
+	var existingCount int
+	err := s.db.QueryRow("SELECT count FROM fruits WHERE name = ?", name).Scan(&existingCount)
+	if err != nil {
+		if err != sql.ErrNoRows {
+			return fmt.Errorf("error checking existing fruit: %w", err)
+		}
+		// if no rows found, fruit doesn't exist => proceed to add a new row
+	} else {
+		// if a row with the same name exists => increase the count and return
+		newCount := existingCount + count
+		_, err := s.db.Exec("UPDATE fruits SET count = ? WHERE name = ?", newCount, name)
+		if err != nil {
+			return fmt.Errorf("error updating existing fruit count: %w", err)
+		}
+		return nil
+	}
+
+	// add a new row with the fruit and the count
 	query := "INSERT INTO fruits (name, count) VALUES (?, ?)"
-	_, err := s.db.Exec(query, name, count)
+	_, err = s.db.Exec(query, name, count)
 	if err != nil {
 		return fmt.Errorf("error adding fruit: %w", err)
 	}
